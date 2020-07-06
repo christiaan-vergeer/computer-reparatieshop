61c724746b0348fd808ec3437586ed0b3b2849d7
chris vergeer <C.H.M.Vergeer@student.tudelft.nl>
2020-07-06 11:42:01 +0200

    Add project files.

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="computer_reparatieshop.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(computer_reparatieshop.Startup))]
+namespace computer_reparatieshop
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
