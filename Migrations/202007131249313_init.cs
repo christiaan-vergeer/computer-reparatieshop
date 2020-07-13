ce84751281bda8b99fcc787dc4d1e2fa373a1cf9
chris vergeer <C.H.M.Vergeer@student.tudelft.nl>
2020-07-13 14:56:33 +0200

    update user input fix1

---
+++ Migrations/202007131249313_init.cs
@@ -0,0 +1,20 @@
+﻿namespace computer_reparatieshop.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class init : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Customers", "EmailAddress", c => c.String());
+            DropColumn("dbo.Customers", "Status");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Customers", "Status", c => c.Int(nullable: false));
+            DropColumn("dbo.Customers", "EmailAddress");
+        }
+    }
+}
